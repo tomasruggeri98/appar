05ffa97042f6d52ae5589a35376715eb55130eb2
tomasruggeri98 <121479475+tomasruggeri98@users.noreply.github.com>
2024-11-12 19:58:20 -0300

    v0.1.0

---
+++ Assets/scripts/ShareScreenShot.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ShareScreenShot : MonoBehaviour
+{
+
+    [SerializeField] private GameObject mainMenuCanvas;
+    private ARPointCloudManager aRPointCloudManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aRPointCloudManager = FindObjectOfType<ARPointCloudManager>();
+    }
+
+
+
+    public void TakeScreenShot()
+    {
+        TurnOnOffARContent();
+        StartCoroutine(TakeScreenshotAndShare());
+    }
+
+    private void TurnOnOffARContent()
+    {
+        var points = aRPointCloudManager.trackables;
+        foreach (var point in points)
+        {
+            point.gameObject.SetActive(!point.gameObject.activeSelf);
+        }
+        mainMenuCanvas.SetActive(!mainMenuCanvas.activeSelf);
+    }
+
+    private IEnumerator TakeScreenshotAndShare()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        string filePath = Path.Combine(Application.temporaryCachePath, "shared img.png");
+        File.WriteAllBytes(filePath, ss.EncodeToPNG());
+
+        // To avoid memory leaks
+        Destroy(ss);
+
+        new NativeShare().AddFile(filePath)
+            .SetSubject("Subject goes here").SetText("Mira como quedaría...")
+            .SetCallback((result, shareTarget) => Debug.Log("Share result: " + result + ", selected app: " + shareTarget))
+            .Share();
+        TurnOnOffARContent ();
+
+        // Share on WhatsApp only, if installed (Android only)
+        //if( NativeShare.TargetExists( "com.whatsapp" ) )
+        //	new NativeShare().AddFile( filePath ).AddTarget( "com.whatsapp" ).Share();
+    }
+}
